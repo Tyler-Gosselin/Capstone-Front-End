ef0e6f18cf40677dd3a12d321bad562c45a38a41
Tyler <gosselin.code@gmail.com>
2021-03-21 15:01:03 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDom from 'react-dom'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import NavContainer from "./Components/Nav/NavContainer"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+      <div>
+        <NavContainer />
+      </div>
+      <h1>Hello world</h1>
+    </Router>
     </div>
   );
 }
---
+++ src/Components/Nav/NavContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom"
+
+function NavContainer() {
+  return (
+    <div className= 'navigation-wrapper'>
+      <NavLink exact to='/'> Home </NavLink>
+      <NavLink exact to='/blog'> Blog </NavLink>
+      <NavLink exact to='/about'> About </NavLink>
+    </div>
+  )
+}
+
+export default NavContainer
