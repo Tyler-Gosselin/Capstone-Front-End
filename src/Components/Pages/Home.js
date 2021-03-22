85f6eb6768ecfe1f5c583a4b7b6765695155d590
Tyler <gosselin.code@gmail.com>
2021-03-22 16:57:10 -0600

    Add basic home styles

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div className= "container">
-      <h1> Home </h1>
+    <div className="home-container">
+      <div className="content-wrapper">
+        <div className="major-news-wrapper">
+          <h1>Major News</h1>
+        </div>
+        <div className="recent-news-wrapper">
+          <h2>Recent News</h2>
+          <div className="news-list">
+            <ul>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit.</li>
+              <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
